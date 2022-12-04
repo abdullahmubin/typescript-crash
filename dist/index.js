a4ec36306b1787a2f59ad9323e648617b316acad
mubinCodemen <codemen.almubin@gmail.com>
2022-12-04 14:53:57 +0600

    basic types and Tuple.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,19 @@
 "use strict";
+//Basic Types
 let id = 5;
+console.log('number: ' + id);
+let nameStr = 'Mubin galib';
+console.log('string: ' + nameStr);
+let isHuman = true;
+console.log('boolean: ' + isHuman);
+let x = 234;
+let ids = [1, 2, 3, 4];
+let idsany = [1, 2, true, 'Test'];
+// Tuple
+let person = [1, 'mubin', true];
+let employee;
+employee = [
+    [1, 'mubin'],
+    [2, 'galib']
+];
+console.log('employee: ', employee);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-let id = 5;
+//Basic Types
+
+let id: number = 5;
+console.log('number: '+ id);
+let nameStr: string = 'Mubin galib'
+console.log('string: ' +nameStr)
+let isHuman: boolean = true;
+console.log('boolean: '+ isHuman);
+let x: any = 234;
+
+let ids:number[] = [1,2,3,4];
+let idsany: any[] = [1,2, true, 'Test'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'mubin', true]
+let employee: [number, string][]
+
+employee = [
+    [1, 'mubin'],
+    [2, 'galib']
+]
+
+console.log('employee: ', employee)
